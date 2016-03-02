357b718521fc538648f9e3495c5c210619723ba6
WY08271 <wy08271@outlook.com>
2016-03-02 16:01:26 +0800

    add replace() test

--- test/main-test.js
+++ test/main-test.js
@@ -3,3 +3,12 @@ describe('splitInput()', function () {
         expect(splitInput('1234')).toEqual(['1', '2', '3', '4']);
     });
 });
+
+describe('replace()', function () {
+    it('can replace numberArrays to shapeArrays', function () {
+        var numberArrays = ['1', '2'];
+        var shapeArrays = [['...', '..|', '..|'], ['._.', '._|', '|_.']];
+
+        expect(replace(numberArrays)).toEqual(shapeArrays);
+    });
+});
