19f11b9d5874337ba25c04c75df5ff84ee58fd8c
WY08271 <wy08271@outlook.com>
2016-03-02 16:29:15 +0800

    format main.js

--- main/main.js
+++ main/main.js
@@ -5,22 +5,22 @@ function splitInput(input) {
 }
 
 
-function replace(numberArrays){
+function replace(numberArrays) {
     var shapes = loadNumberShapes();
     var shapeArrays = [];
 
-    numberArrays.forEach(function(numberArray){
+    numberArrays.forEach(function (numberArray) {
         shapeArrays.push(shapes[numberArray]);
     });
 
     return shapeArrays;
 }
 
-function print(shapeArrays){
+function print(shapeArrays) {
     var shapeString = '';
 
-    for(var i = 0; i < 3; i++){
-        for(var k = 0 ; k < shapeArrays.length; k++){
+    for (var i = 0; i < 3; i++) {
+        for (var k = 0; k < shapeArrays.length; k++) {
             shapeString += shapeArrays[k][i] + ' ';
         }
         shapeString += '\n';
