d44360c58bf0f91e1cea4a4fe6a123dfc62a79a0
WY08271 <wy08271@outlook.com>
2016-03-02 16:25:32 +0800

    add print() function

--- main/main.js
+++ main/main.js
@@ -15,3 +15,16 @@ function replace(numberArrays){
 
     return shapeArrays;
 }
+
+function print(shapeArrays){
+    var shapeString = '';
+
+    for(var i = 0; i < 3; i++){
+        for(var k = 0 ; k < shapeArrays.length; k++){
+            shapeString += shapeArrays[k][i] + ' ';
+        }
+        shapeString += '\n';
+    }
+
+    return shapeString;
+}
