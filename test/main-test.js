03de303a494f52bc9eab09e542c1465b07c04165
WY08271 <wy08271@outlook.com>
2016-03-02 16:46:19 +0800

    add lcdPrint() test

--- test/main-test.js
+++ test/main-test.js
@@ -21,3 +21,16 @@ describe('print()', function () {
         expect(print(shapeArrays)).toEqual(shapeString);
     });
 });
+
+describe('lcdPrint()', function () {
+    it('can change number to shape', function () {
+        var shapeString = "... ._. ._. ... ._. ._. ._. ._. ._. ._. \n"
+            + "..| ._| ._| |_| |_. |_. ..| |_| |_| |.| \n"
+            + "..| |_. ._| ..| ._| |_| ..| |_| ..| |_| \n";
+
+        spyOn(console, 'log');
+        lcdPrint('1234567890');
+
+        expect(console.log).toHaveBeenCalledWith(shapeString);
+    });
+});
